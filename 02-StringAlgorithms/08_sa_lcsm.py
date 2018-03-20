fbc2f767099b70cdd73793a9d6ed80b6cf828463
Giancarlo Garbagnati <giancarlo.garbagnati@gmail.com>
2018-03-19 23:46:06 -0700

    soln to lcsm

--- 02-StringAlgorithms/08_sa_lcsm.py
+++ 02-StringAlgorithms/08_sa_lcsm.py
@@ -24,7 +24,7 @@ AC
 # Building file name
 probnum = 'lcsm'
 filename = '../data/rosalind_' + probnum + '.txt'
-filename = '../data/rosalind_' + probnum + '_sample.txt'
+#filename = '../data/rosalind_' + probnum + '_sample.txt'
 
 # Parse out all the label/dna pairs from the file into a dictionary
 def fasta_to_dict(filename):
@@ -59,4 +59,48 @@ fasta_dict = fasta_to_dict(filename)
 #  we don't find a match. When that happens, we look at all substrings of -1 the previous len,
 #  and compare to all others... repeat this until we are able to find one that matches all DNA
 
-print(fasta_dict)
+# Let's find the smallest DNA, then remove that sequence from the dictionary
+smallest_key, smallest_sequence = fasta_dict.popitem()
+fasta_dict[smallest_key] = smallest_sequence
+for key, sequence in fasta_dict.items():
+    if len(smallest_sequence) > len(sequence):
+        smallest_sequence = sequence
+        smallest_key = key
+fasta_dict.pop(smallest_key)
+
+# Returns all substrings in a sequence s of len l (l must be >= len(s))
+def all_substrings(s, l):
+    if len(s) < l:
+        print("Error: 'l' must be <= len(s)")
+        return []
+    num_sub_strings = len(s) - l + 1
+    sub_strings = []
+    for i in range(0,num_sub_strings):
+        sub_strings.append(s[i:i+l])
+    return sub_strings
+
+# Go through each len (decresing) of the smallest_sequence and check all the substrings against
+#  all other sequences
+longest_substring = ''
+longest_substring_found = False
+match = True
+for i in range(len(smallest_sequence), 0, -1):
+    substrings = all_substrings(smallest_sequence,i)
+    for substring in substrings:
+        for value in fasta_dict.values():
+            if substring not in value:
+                match = False
+                break
+        if match:
+            longest_substring_found = True
+            longest_substring = substring
+            break
+            # We've found the substring, so we leave all loops
+        else:
+            match = True
+            # Continue looking with this group of substrings (or move onto -1 len substrings)
+    if longest_substring_found:
+        break
+
+print(longest_substring)
+
